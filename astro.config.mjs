21fa4ccfd863d5ae9cb6578c5021e949af1138fe
Victor Diaz <75600392+victorzdev@users.noreply.github.com>
2024-01-30 00:49:10 -0500

    config(astro): add Partytown library

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ]
 });
