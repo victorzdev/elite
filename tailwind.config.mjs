3663b654c6251b854ad69036a6f21a7d3f7b7d82
Victor Diaz <75600392+victorzdev@users.noreply.github.com>
2024-04-11 19:45:21 -0400

    feat: add custom colors and enable TypeScript type checking in Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				blue: {
+					500: '#007ACC',
+					600: '#003366',
+				},
+				orange: {
+					600: '#f59940',
+				},
+				red: {
+					500: '#F58B7D',
+				},
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
